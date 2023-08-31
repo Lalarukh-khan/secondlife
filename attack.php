33616af07e42a3022ff0b687c08dda6dc31da52d
Lalarukh Khan <lalarukhkhan179@gmail.com>
2023-08-31 14:17:34 +0500

    Attack PHP saved

--- attack.php
+++ attack.php
@@ -1,40 +1,7 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['attacker']) && isset($_POST['defender'])) {
-    $servername = "localhost";
-	$username = "elbkmjmy_WPGXP";
-	$password = "1#VghAm&}-.>uS)H=";
-	$dbname = "elbkmjmy_WPGXP";
-	// Retrieve the attacker and defender names from the request
-	$attacker = $_POST['attacker'];
-	$defender = $_POST['defender'];
-
-	// Store the attacker and defender names in the database
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-
-	$sql = "INSERT INTO attacks (attacker, defender) VALUES ('$attacker', '$defender')";
-	if ($conn->query($sql) === TRUE) {
-		echo "Attack data stored successfully.";
-
-		// Send response back to Second Life
-		echo "Healing will be paused for the next 2 hours.";
-	} else {
-		echo "Error: " . $sql . "<br>" . $conn->error;
-	}
-
-	$conn->close();
-}
-
-?>
-
-
-
-<?php
-if (isset($_POST['attacker']) && isset($_POST['defender'])) {
-    $attacker = $_POST['attacker'];
-    $defender = $_POST['defender'];
+if (isset($_GET['attacker']) && isset($_GET['defender'])) {
+    $attacker = $_GET['attacker'];
+    $defender = $_GET['defender'];
 
 	$conn = new mysqli("localhost", "elbkmjmy_WPGXP", "1#VghAm&}-.>uS)H=", "elbkmjmy_WPGXP");
 	if ($conn->connect_error) {
@@ -44,9 +11,7 @@ if (isset($_POST['attacker']) && isset($_POST['defender'])) {
 	$sql = "INSERT INTO attacks (attacker, defender) VALUES ('$attacker', '$defender')";
 	if ($conn->query($sql) === TRUE) {
 		echo "Attack data stored successfully.";
-
-		// Send response back to Second Life
-		echo "Healing will be paused for the next 2 hours.";
+		echo 'The attacker is: '.$attacker.' and the defender is: '.$defender;
 	} else {
 		echo "Error: " . $sql . "<br>" . $conn->error;
 	}
@@ -54,5 +19,8 @@ if (isset($_POST['attacker']) && isset($_POST['defender'])) {
 	$conn->close();
     
     echo "Players' data stored.";
+}
+else{
+    echo "Fail begin here!!!";
 }
 ?>
