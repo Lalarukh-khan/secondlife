3155a4d88c3eb96ecf6d19f94c72c437d2f76cdc
Lalarukh Khan <lalarukhkhan179@gmail.com>
2023-09-02 15:07:10 +0500

    Health and attack server side code

--- attack.php
+++ attack.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 if (isset($_GET['attacker']) && isset($_GET['defender'])) {
     $attacker = $_GET['attacker'];
     $defender = $_GET['defender'];
@@ -7,18 +9,196 @@ if (isset($_GET['attacker']) && isset($_GET['defender'])) {
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
+$query = "SELECT * FROM attacks WHERE attacker = '$attacker' AND defender = '$defender'";
+$result = $conn->query($query);
 
-	$sql = "INSERT INTO attacks (attacker, defender) VALUES ('$attacker', '$defender')";
-	if ($conn->query($sql) === TRUE) {
-		echo "Attack data stored successfully.";
-		echo 'The attacker is: '.$attacker.' and the defender is: '.$defender;
-	} else {
-		echo "Error: " . $sql . "<br>" . $conn->error;
-	}
+if ($result->num_rows > 0) {
+    // echo "Defender and Attacker already exist";
+    while ($row = $result->fetch_assoc()) {
+        if ($row['attacker_health'] > 0 && $row['defender_health'] > 0) {
+            $attackerpoint = $row['attacker_points'] + 1;
+            $randomNumber = mt_rand(1, 20);
+            $randomNumber1 = $randomNumber + $attackerpoint;
+            $randomNumber2 = mt_rand(1, 20);
+            echo "The attacker number is: ".$randomNumber1;
+            echo " The defender number is: ".$randomNumber2;
+            echo " Your health has been paused for 2 hours.";
+            $currenttime = time();
+            $timestampAfter = strtotime('+1 second', $currenttime);
+        	$formattedCurrentTime = date('Y-m-d H:i:s', $timestampAfter);
+        	if($row['defender_last_updated'] !== null){
+                $defendertime = $row['defender_last_updated'];
+                $timestampAfter2 = strtotime($defendertime);
+            	$formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2);
+                if ($formattedCurrentTime > $formattedTimestamp) {
+                    $updateQuery = "UPDATE attacks SET defender_health = 6 WHERE attacker = '$attacker' AND defender = '$defender'";
+                    $conn->query($updateQuery);
+                }
+        	}
+        	if($row['attacker_last_updated'] !== null){
+        	    $attackertime = $row['attacker_last_updated'];
+                $timestampAfter3 = strtotime($attackertime);
+            	$formattedTimestamp2 = date('Y-m-d H:i:s', $timestampAfter3);
+                if ($formattedCurrentTime > $formattedTimestamp2) {
+                    $updateQuery = "UPDATE attacks SET attacker_health = 6 WHERE attacker = '$attacker' AND defender = '$defender'";
+                    $conn->query($updateQuery);
+                }
+        	}
+            $updateQuery = "UPDATE attacks SET attacker_number = '$randomNumber1', defender_number = '$randomNumber2', attacker_points = '$attackerpoint' WHERE attacker = '$attacker' AND defender = '$defender'";
+            $conn->query($updateQuery);
+            if($randomNumber1 < 5){
+                $getattackerhealth  = $row['attacker_health'];
+                $getattackerhealth -= 1;
+                $timestampFromDatabase = time();
+                $timestampAfter2Hours = strtotime('+2 hours', $timestampFromDatabase); 
+                $formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2Hours);
+                $updatehealth = "UPDATE attacks SET attacker_health = '$getattackerhealth', attacker_last_updated = '$formattedTimestamp' WHERE attacker = '$attacker' AND defender = '$defender'";
+                $conn->query($updatehealth);
+                echo " Attacker Lost the Attack due to low Attack!";
+                // STEP 3 CALIING for attacker
+            }
+            elseif($randomNumber2 < 5){
+                $getdefenderhealth  = $row['defender_health'];
+                $getdefenderhealth -= 1;
+                $timestampFromDatabase = time();
+                $timestampAfter2Hours = strtotime('+2 hours', $timestampFromDatabase); 
+                $formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2Hours);
+                $updatehealth = "UPDATE attacks SET defender_health = '$getdefenderhealth', defender_last_updated = '$formattedTimestamp' WHERE attacker = '$attacker' AND defender = '$defender'";
+                $conn->query($updatehealth);
+                echo " Defender Lost due to low defence!";
+                // STEP 3 CALIING for defender
+            }
+            else{
+                if($randomNumber1 > $randomNumber2){
+                $getdefenderhealth  = $row['defender_health'];
+                $getdefenderhealth -= 1;
+                $timestampFromDatabase = time();
+                $timestampAfter2Hours = strtotime('+2 hours', $timestampFromDatabase); 
+                $formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2Hours);
+                $updatehealth = "UPDATE attacks SET defender_health = '$getdefenderhealth', defender_last_updated = '$formattedTimestamp' WHERE attacker = '$attacker' AND defender = '$defender'";
+                $conn->query($updatehealth);
+                    echo "  '$attacker' either hits or misses!";
+                    // STEP 3 CALIING for defender
+                }
+                else{
+                $getattackerhealth  = $row['attacker_health'];
+                $getattackerhealth -= 1;
+                $timestampFromDatabase = time();
+                $timestampAfter2Hours = strtotime('+2 hours', $timestampFromDatabase); 
+                $formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2Hours);
+                $updatehealth = "UPDATE attacks SET attacker_health = '$getattackerhealth', attacker_last_updated = '$formattedTimestamp' WHERE attacker = '$attacker' AND defender = '$defender'";
+                $conn->query($updatehealth);
+                echo "  Attacker Lost the Attack due to low Attack!";
+                    echo "  '$defender' won by greater defence";
+                    // STEP 3 CALIING for attacker
+                }
+                
+            }
+            
+        } else {
+            echo " Player has no health!";
+        }
+    }
+} else {
+    // Row doesn't exist, insert a new row
+    $insertQuery = "INSERT INTO attacks (attacker, defender, attacker_health, defender_health) VALUES ('$attacker', '$defender', 6, 6)";
+    
+    if ($conn->query($insertQuery) === TRUE) {
+        $newRowId = $conn->insert_id;
+
+    // SQL query to select the newly inserted row by its ID
+    $selectQuery = "SELECT * FROM attacks WHERE id = $newRowId";
+    $result = $conn->query($selectQuery);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $attackerpoint = $row['attacker_points'] + 1;
+            $randomNumber = mt_rand(1, 20);
+            $randomNumber1 = $randomNumber + $attackerpoint;
+            $randomNumber2 = mt_rand(1, 20);
+            echo "The attacker number is: ".$randomNumber1;
+            echo " The defender number is: ".$randomNumber2;
+            echo " Your health has been paused for 2 hours.";
+            $currenttime = time();
+            $timestampAfter = strtotime('+1 second', $currenttime);
+        	$formattedCurrentTime = date('Y-m-d H:i:s', $timestampAfter);
+        	if($row['defender_last_updated'] !== null){
+                $defendertime = $row['defender_last_updated'];
+                $timestampAfter2 = strtotime($defendertime);
+            	$formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2);
+                if ($formattedCurrentTime > $formattedTimestamp) {
+                    $updateQuery = "UPDATE attacks SET defender_health = 6 WHERE attacker = '$attacker' AND defender = '$defender'";
+                    $conn->query($updateQuery);
+                }
+        	}
+        	if($row['attacker_last_updated'] !== null){
+        	    $attackertime = $row['attacker_last_updated'];
+                $timestampAfter3 = strtotime($attackertime);
+            	$formattedTimestamp2 = date('Y-m-d H:i:s', $timestampAfter3);
+                if ($formattedCurrentTime > $formattedTimestamp2) {
+                    $updateQuery = "UPDATE attacks SET attacker_health = 6 WHERE attacker = '$attacker' AND defender = '$defender'";
+                    $conn->query($updateQuery);
+                }
+        	}
+            $updateQuery = "UPDATE attacks SET attacker_number = '$randomNumber1', defender_number = '$randomNumber2', attacker_points = '$attackerpoint' WHERE attacker = '$attacker' AND defender = '$defender'";
+            $conn->query($updateQuery);
+            if($randomNumber1 < 5){
+                $getattackerhealth  = $row['attacker_health'];
+                $getattackerhealth -= 1;
+                $timestampFromDatabase = time();
+                $timestampAfter2Hours = strtotime('+2 hours', $timestampFromDatabase); 
+                $formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2Hours);
+                $updatehealth = "UPDATE attacks SET attacker_health = '$getattackerhealth', attacker_last_updated = '$formattedTimestamp' WHERE attacker = '$attacker' AND defender = '$defender'";
+                $conn->query($updatehealth);
+                echo " Attacker Lost the Attack due to low Attack!";
+                // STEP 3 CALIING for attacker
+            }
+            elseif($randomNumber2 < 5){
+                $getdefenderhealth  = $row['defender_health'];
+                $getdefenderhealth -= 1;
+                $timestampFromDatabase = time();
+                $timestampAfter2Hours = strtotime('+2 hours', $timestampFromDatabase); 
+                $formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2Hours);
+                $updatehealth = "UPDATE attacks SET defender_health = '$getdefenderhealth', defender_last_updated = '$formattedTimestamp' WHERE attacker = '$attacker' AND defender = '$defender'";
+                $conn->query($updatehealth);
+                echo " Defender Lost due to low defence!";
+                // STEP 3 CALIING for defender
+            }
+            else{
+                if($randomNumber1 > $randomNumber2){
+                $getdefenderhealth  = $row['defender_health'];
+                $getdefenderhealth -= 1;
+                $timestampFromDatabase = time();
+                $timestampAfter2Hours = strtotime('+2 hours', $timestampFromDatabase); 
+                $formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2Hours);
+                $updatehealth = "UPDATE attacks SET defender_health = '$getdefenderhealth', defender_last_updated = '$formattedTimestamp' WHERE attacker = '$attacker' AND defender = '$defender'";
+                $conn->query($updatehealth);
+                    echo " '$defender' either hits or misses!";
+                    // STEP 3 CALIING for defender
+                }
+                else{
+                $getattackerhealth  = $row['attacker_health'];
+                $getattackerhealth -= 1;
+                $timestampFromDatabase = time();
+                $timestampAfter2Hours = strtotime('+2 hours', $timestampFromDatabase); 
+                $formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2Hours);
+                $updatehealth = "UPDATE attacks SET attacker_health = '$getattackerhealth', attacker_last_updated = '$formattedTimestamp' WHERE attacker = '$attacker' AND defender = '$defender'";
+                $conn->query($updatehealth);
+                echo " Attacker Lost the Attack due to low Attack!";
+                    echo " '$attacker' won by greater defence";
+                    // STEP 3 CALIING for attacker
+                }
+                
+            }
+            
+        
+    }
+    }
+    } else {
+        echo " Error inserting new row: " . $conn->error;
+    }
+}
 
 	$conn->close();
-    
-    echo "Players' data stored.";
 }
 else{
     echo "Fail begin here!!!";
--- healthCounter.php
+++ healthCounter.php
@@ -1,29 +1,102 @@
 <?php
 // Database connection parameters
-$servername = "localhost";
-$username = "root";
-$password = "mysql";
-$dbname = "secondlife";
+$host = 'localhost';
+$username = 'root';
+$password = 'mysql';
+$database = 'secondlife';
 
-// Create a connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create a database connection
+$conn = new mysqli($host, $username, $password, $database);
 
-// Check connection
+// Check if the connection was successful
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die('Connection failed: ' . $conn->connect_error);
+}
+echo "HELOOOOOOO <br>";
+// Define the time interval in seconds (1 minute)
+$interval = 60; // 1 minute
+
+$timestampFromDatabase1 = time(); // Get the current timestamp
+$timestampAfter2Hours = strtotime('+2 hours', $timestampFromDatabase1); // Add 2 hours
+
+// Format the new timestamp as needed (e.g., "Y-m-d H:i:s")
+$formattedTimestamp = date('Y-m-d H:i:s', $timestampAfter2Hours);
+
+echo "Final time : ". $formattedTimestamp;
+// Retrieve the current state of the number and last updated timestamp
+$query = "SELECT number, last_updated FROM number_state";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $number = $row['number'];
+	$timestamp = strtotime($row['last_updated']);
+	$dbTime = strtotime($row['last_updated']); // Convert the database time to a timestamp
+    
+    // Get the current time as a timestamp
+    $currentTime = time();
+	$formattedTimestamp = date('Y-m-d H:i:s', $dbTime);
+	$formattedCurrentTime = date('Y-m-d H:i:s', $currentTime);
+    echo "currentTime : ".$formattedCurrentTime;
+    echo "dbTime : ".$formattedTimestamp;
+    // Check if the current time is ahead of the database time
+    if ($currentTime > $dbTime) {
+        echo "Current time is ahead of the database time.";
+    } else {
+        echo "Current time is not ahead of the database time.";
+    }
+
+    // $currentTime = time();
+	// // $timestamp = strtotime($row['last_updated']);
+	// $timestamp = strtotime($row['last_updated']);
+
+	// if ($timestamp && getTimeDifference($timestamp) >= 60) {
+	// 	echo "Current time is 1 minute or more ahead of the timestamp.";
+	// }else {
+    //     echo "Less than 1 minute has passed since the stored timestamp.";
+    // }
+    // Check if it's been more than 1 minute since the last update
+    // if (time() - $lastUpdated >= $interval) {
+    //     // It's been more than 1 minute, so increment the number by 1
+    //     $number += 1;
+
+    //     // Update the database with the new number and current timestamp
+    //     $updateQuery = "UPDATE number_state SET number = 6, last_updated = NOW()";
+    //     $conn->query($updateQuery);
+    // }
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Button was clicked, decrement the value by 1
-    $sql = "UPDATE number_state SET number = number - 1";
-    $conn->query($sql);
+function getTimeDifference($timestamp) {
+	$currentTimestamp = time();
+	return ($currentTimestamp - $timestamp) / 60; // Return the difference in minutes
+}
 
-    // Start a timer for 1 minute (60 seconds)
-    sleep(60);
+// Check if the button was clicked
+if (isset($_POST['button_clicked'])) {
+    // Decrease the number by 1
+    $number -= 1;
 
-    // Increment the value by 1
-    $sql = "UPDATE number_state SET number = number + 1";
-    $conn->query($sql);
+    // Update the database with the new number and current timestamp
+	
+	$query = "SELECT last_updated FROM number_state"; // Add your condition here
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $timestampFromDatabase = strtotime($row['last_updated']);
+		$newTimestamp = date('Y-m-d H:i:s', strtotime('+1 minute', $timestampFromDatabase));
+        
+        // Update the database with the new timestamp
+        $updateQuery = "UPDATE number_state SET last_updated = '$newTimestamp'"; // Add your condition here
+        $conn->query($updateQuery);
+        
+        echo "1 minute added to the timestamp_column.";
+    } else {
+        echo "No matching rows found.";
+    }
+
+    // $updateQuery = "UPDATE number_state SET number = 6, last_updated = NOW()";
+    // $conn->query($updateQuery);
 }
 
 // Close the database connection
@@ -33,11 +106,12 @@ $conn->close();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Button Example</title>
+    <title>Number Update</title>
 </head>
 <body>
+    <h1>Current Number: <?php echo $number; ?></h1>
     <form method="post">
-        <input type="submit" value="Click to Decrease Number">
+        <input type="submit" name="button_clicked" value="Decrease Number">
     </form>
 </body>
 </html>
