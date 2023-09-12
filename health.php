084217b64effeb0519fb42ad58ea7b66d03d86e2
Lalarukh Khan <lalarukhkhan179@gmail.com>
2023-09-12 16:17:43 +0500

    Health check added

--- health.php
+++ health.php
@@ -15,33 +15,98 @@ if (isset($_GET['player'])) {
     // $dresult = $conn->query($dquery);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            echo "Profile Found as attacker!";
-            $dbTime = strtotime($row['attacker_last_updated']); 
+            echo "Profile found to check health!"; 
+            $health = $row['attacker_health']; 
+            echo "Before checking your current stored health is: ".$health;
             $currentTime = time();
-            echo "current time: ".$currentTime;
-            echo "Attacker time: ".$dbTime;
+            $dbTime = strtotime($row['attacker_last_updated']);
             $timeDifferenceInSeconds = $currentTime - $dbTime;
             if ($timeDifferenceInSeconds >= 1800 && $timeDifferenceInSeconds < 5400) {
-                echo "Current time is 30 minutes ahead of DB time.";
+                if($health < 6){
+                    $health += 1;
+                    echo "1 point of health has been regenerated. As '30 minutes' has been passed away.";
+                    $updateQuery = "UPDATE attacks SET attacker_health = '$health' WHERE attacker = '$player'";
+                    $conn->query($updateQuery);
+                    echo "Your current health is: '$health'";
+                }
+                else{
+                    echo "Your health is already full. You have '$health' points of health.";
+                }
             }
             elseif ($timeDifferenceInSeconds >= 5400 && $timeDifferenceInSeconds < 10800) {
-                echo "Current time is 1 hour and 30 mins ahead of DB time.";
+                if($health < 6){
+                    $health += 2;
+                    echo "2 points of health has been regenerated. As '1 hour' has been passed away.";
+                    if($health > 6){
+                        $health = 6;
+                    }
+                    $updateQuery = "UPDATE attacks SET attacker_health = '$health' WHERE attacker = '$player'";
+                    $conn->query($updateQuery);
+                    echo "Your current health is: '$health'";
+                }
+                else{
+                    echo "Your health is already full. You have '$health' points of health.";
+                }
             }
             elseif ($timeDifferenceInSeconds >= 10800 && $timeDifferenceInSeconds < 18000) {
-                echo "Current time is 3 hours ahead of DB time.";
+                if($health < 6){
+                    $health += 3;
+                    echo "3 points of health has been regenerated. As '3 hours' has been passed away.";
+                    if($health > 6){
+                        $health = 6;
+                    }
+                    $updateQuery = "UPDATE attacks SET attacker_health = '$health' WHERE attacker = '$player'";
+                    $conn->query($updateQuery);
+                    echo "Your current health is: '$health'";
+                }
+                else{
+                    echo "Your health is already full. You have '$health' points of health.";
+                }
             }
             elseif ($timeDifferenceInSeconds >= 18000 && $timeDifferenceInSeconds < 25200) {
-                echo "Current time is 5 hours ahead of DB time.";
+                if($health < 6){
+                    $health += 4;
+                    echo "4 points of health has been regenerated. As '5 hours' has been passed away.";
+                    if($health > 6){
+                        $health = 6;
+                    }
+                    $updateQuery = "UPDATE attacks SET attacker_health = '$health' WHERE attacker = '$player'";
+                    $conn->query($updateQuery);
+                    echo "Your current health is: '$health'";
+                }
+                else{
+                    echo "Your health is already full. You have '$health' points of health.";
+                }
             }
             elseif ($timeDifferenceInSeconds >= 25200 && $timeDifferenceInSeconds < 28800) {
-                echo "Current time is 7 hours ahead of DB time.";
+                if($health < 6){
+                    $health += 5;
+                    echo "5 points of health has been regenerated. As '7 hours' has been passed away.";
+                    if($health > 6){
+                        $health = 6;
+                    }
+                    $updateQuery = "UPDATE attacks SET attacker_health = '$health' WHERE attacker = '$player'";
+                    $conn->query($updateQuery);
+                    echo "Your current health is: '$health'";
+                }
+                else{
+                    echo "Your health is already full. You have '$health' points of health.";
+                }
             }
             elseif ($timeDifferenceInSeconds >= 28800) {
-                echo "Current time is 8 hours ahead of DB time.";
+                if($health < 6){
+                    $health = 6;
+                    echo "6 points of health has been regenerated. As '8 hours' has been passed away. Now your complete health is regenerated.";
+                    $updateQuery = "UPDATE attacks SET attacker_health = '$health' WHERE attacker = '$player'";
+                    $conn->query($updateQuery);
+                    echo "Your current health is: '$health'";
+                }
+                else{
+                    echo "Your health is already full. You have '$health' points of health.";
+                }
             } else {
-                echo "Current time is not 30 minutes, 2 hours, or 3 hours ahead of DB time.";
+                echo "Your health can not be regeneted, as its still paused due to less time. After 30 mins, your health will begin regenerating.";
             }
-            echo "Player current health is: ".$row['attacker_health'];
         }
     }
     // elseif ($dresult->num_rows > 0) {
